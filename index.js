848c15a3c5cd124c6379e108d63c86e942818b93
Nilesh Paradva <nilesh-paradva>
2025-01-31 11:34:56 +0530

    Volunteer Registration Form

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const bodyParser = require("body-parser");
+const port = 3001;
+
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+let localData = [];
+
+app.get("/", (req, res) => {
+    res.render("index", {localData});
+});
+
+app.post("/add", (req, res) => {
+    const  {name, email, phone, skills}  = req.body;
+    
+    const obj = {
+        name,
+        email,
+        phone,
+        skills,
+        id: Math.floor(Math.random() * 10000)
+    };
+    localData.push(obj);
+    res.redirect("/");
+    console.log("volunteer added");
+});
+
+app.get("/update/:id", (req, res) => {
+    const item = localData.find((item) => item.id == req.params.id);
+    res.render("edit", {item});
+});
+
+app.post("/update/:id", (req, res) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+    localData = localData.map((item) => item.id == id ? {...item, ...updatedData} : item);
+    res.redirect("/");
+    console.log("volunteer updated");
+    
+});
+
+app.get("/delete/:id", (req, res) => {
+    localData = localData.filter((item) => item.id != req.params.id);
+    res.redirect("/");
+    console.log("volunteer deleted");
+});
+
+app.listen(port, (req, res) => {
+    console.log(`Server is running on port ${port}`, `http://localhost:${port}`);
+});
